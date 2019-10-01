0aa36df3ef6962c516e1fc9f419b49773efa031d
collinsbett29 <collinsbett29@gmail.com>
2019-10-01 11:00:02 +0300

    update src file

---
+++ src/app/github-service/github.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import  { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+  private username = 'collinsbett29';
+  private client_id = 'b491b07269943f16fd21';
+  private client_secret = '7a258b7f4a4a36be20f056e7338142201f14e64f';
+
+  constructor(private _http:HttpClient) {
+    console.log('Github Service init......')
+   }
+
+   getUser(username){
+     console.log(username);
+      return this._http.get('https://api.github.com/users/' + username)
+      .pipe(map(res => res));
+   }
+
+   getRepos(username){
+    console.log(username);
+     return this._http.get('https://api.github.com/users/' + username + '/repos')
+     .pipe(map(res => res));
+  }
+
+  updateUsername(username:string){
+    this.username = username;
+
+  }
+
+   
+}
+
---
+++ src/app/github/github.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github-service/github.service';
+
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
+export class GithubComponent implements OnInit {
+  user:any;
+  repos: any;
+  username:string;
+
+  constructor(private _githubService:GithubService) {
+    console.log('Collins KIpkorir Bett...')
+  }
+
+    search(username){
+      this._githubService.updateUsername(this.username);
+      
+      this._githubService.getUser(username).subscribe(user => {
+        // console.log(user);
+        this.user = user;
+      });
+  
+      this._githubService.getRepos(username).subscribe(repos => {
+        console.log(repos);
+        this.repos = repos;
+       });
+   }
+
+  ngOnInit() {
+    this._githubService.getUser('username').subscribe(user => {
+      console.log(user);
+      this.user = user;
+    });
+
+    this._githubService.getRepos('username').subscribe(repos => {
+        console.log(repos);
+        this.repos = repos;
+       });
+  }
+
+}
